fe7f14c86e69791d0979e00f3902daa37f9418cf
zainaly0 <zaidk4076@gmail.com>
2024-08-29 22:58:17 +0530

    create party list complete

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -12,7 +12,10 @@ class PartyController extends Controller
    public function index()
    {
 
-      $parties = Party::all();
+      // $parties = Party::all();
+
+      $parties = Party::select('id', 'party_name', "full_name", 'phone_no', "address", "account_holder_name", "account_no", "bank_name", "ifsc_code", "branch_address", "created_At");
+
       return view('party.index', compact('parties'));
    }
 
@@ -29,7 +32,7 @@ class PartyController extends Controller
       $req->validate([
          'party_name' => 'required',
          'full_name' => 'required|string|min:2|max:20',
-         'phone_no'=> 'required|numeric|min:10|max:10',
+         'phone_no' => 'required|numeric|min:10|max:10',
          'address' => 'required|max:255',
 
          "account_holder_name" => 'required|string|min:2|max:20',
