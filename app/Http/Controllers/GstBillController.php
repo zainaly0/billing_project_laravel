88a518478755fdbc930f76d2a730790ea0ea16a4
zainaly0 <zaidk4076@gmail.com>
2024-08-30 00:12:07 +0530

    gst bill module complete

--- app/Http/Controllers/GstBillController.php
+++ app/Http/Controllers/GstBillController.php
@@ -3,21 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GstBill;
+use App\Models\Party;
+use Illuminate\Support\Facades\DB;
+
 
 class GstBillController extends Controller
 {
-    public function addGstBill()
-    {
-        return view('gst-bill.index');
-    }
-
     public function index()
     {
-        return view('gst-bill.add');
+        $bills = GstBill::with('party')->get();
+        return view('gst-bill.index', compact('bills'));
     }
 
-    public function print()
+    public function addGstBill()
     {
-        return view('gst-bill.print');
+        $data['parties'] = Party::where('party_type', 'client')->orderBy('full_name')->get();
+        return view('gst-bill.add', $data);
+    }
+
+    public function createGstBill(Request $request)
+    {
+        $request->validate([
+            'party_id' => 'required|exists:parties,id',
+            'invoice_data' => 'required|date',
+            'invoice_no' => 'required|string|max:255',
+            'item_description'=> 'required|max:250',
+            'total_amount' => 'required|numeric',
+            'cgst_rate' => 'nullable|min:0|max:100',
+            'cgst_amount'=> 'numeric|min:0',
+            'sgst_rate' => 'nullable|min:0|max:100',
+            'sgst_amount'=> 'numeric|min:0',
+            'igst_rate' => 'nullable|min:0|max:100',
+            'igst_amount'=> 'numeric|min:0',
+            'tax_amount' => 'nullable|min:0',
+            'net_amount'=> 'numeric|numeric|min:0',
+        ]);
+
+        $param = $request->all();
+
+        unset($param['_token']);
+        GstBill::create($param);
+        return redirect()->route('manage-gst-bill')->withStatus("bill created successfully");
+    }
+
+    public function print($id){
+        $data['bill'] = GstBill::where('id', $id)->with('party')->first();
+        return view('gst-bill.print', $data);
     }
 }
